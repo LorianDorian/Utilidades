362b7f7cad2d23156684e04de3da40cd5ae2b1de
LorianDorian <andre.alderete.h@gmail.com>
2024-04-22 23:17:36 -0600

    update

--- Utilidades/Objetos.cs
+++ Utilidades/Objetos.cs
@@ -1,13 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Text.Json;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Utilidades
 {
     public class Objetos
     {
+        public static string SerializeObject<T>(T objectToSerialize)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            StringWriter writer = new StringWriter();
+            serializer.Serialize(writer, objectToSerialize);
+            return writer.ToString();
+        }
+
+        public static string SerializeObject(object obj)
+        {
+            return Serialize(obj);
+        }
+
+        public static byte[] SerializeToBinary(object obj)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(memoryStream, obj);
+                return memoryStream.ToArray();
+            }
+        }
+
 
     }
 }
