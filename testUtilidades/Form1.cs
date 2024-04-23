a41a654b222b2887cebdd14c8548823e2ce15f11
LorianDorian <andre.alderete.h@gmail.com>
2024-04-23 17:57:53 -0600

    added new forms, finishes archives text

---
+++ testUtilidades/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Utilidades;
+
+namespace testUtilidades
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 FormArchivos = new Form2();
+            FormArchivos.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form3 FormColecciones = new Form3();
+            FormColecciones.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form4 FormCadenas = new Form4();
+            FormCadenas.ShowDialog();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form5 FormConversionTipos = new Form5();
+            FormConversionTipos.ShowDialog();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form6 FormDatos = new Form6();
+            FormDatos.ShowDialog();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Form7 FormFechas = new Form7();
+            FormFechas.ShowDialog();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Form8 FormHash = new Form8();
+            FormHash.ShowDialog();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Form9 FormMate = new Form9();
+            FormMate.ShowDialog();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Form10 FormNumAleatorios = new Form10();
+            FormNumAleatorios.ShowDialog();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Form10 FormObjetos = new Form10();
+            FormObjetos.ShowDialog();
+        }
+    }
+}
---
+++ testUtilidades/Form2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Utilidades;
+using System.IO;
+using System.IO.Compression;
+
+namespace testUtilidades
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Archivos.CreateTextFile(textBox1.Text, richTextBox1.Text);
+
+            try
+            {
+                if (File.Exists(textBox1.Text))
+                {
+                    label4.Text = "¡Archivo Creado!";
+                }
+            }
+            catch
+            {
+                label4.Text = "Hubo un error creando el archivo. Intenta nuevamente.";
+            }
+        }
+    }
+}
